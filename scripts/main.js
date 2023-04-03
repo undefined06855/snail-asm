acde9f8b6b9f05c4045fbc162cdd78535293fc4e
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2023-04-03 07:52:28 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -3,27 +3,9 @@ textarea.addEventListener("keydown", event => {
     {
         if (event.code == "Enter")
         {
-            if (event.ctrlKey && !event.shiftKey)
-            {
-                runType = "normal"
-                startInterpret()
-                event.preventDefault()
-            }
-            else if (event.ctrlKey && event.shiftKey)
-            {
-                if (runType == "step")
-                {
-                    requestAnimationFrame(cycle)
-                }
-                else
-                {
-                    runType = "step"
-                    startInterpret()
-                    event.preventDefault()
-                }
-            }
-            // newline: do stuf:
-            else if (getLastLineUserWasOn().charAt(0) == ".")
+            const exitKeywords = ["rts", "jmp", "jsr"]
+
+            if (getLastLineUserWasOn().charAt(0) == ".")
             {
                 // just typed a subroutine char or something
                 autoType("\n")
@@ -32,7 +14,7 @@ textarea.addEventListener("keydown", event => {
     
                 event.preventDefault()
             }
-            else if (getLastLineUserWasOn().substring(0, tabSize + 3) == " ".repeat(tabSize) + "rts" || getLastLineUserWasOn().substring(0, tabSize + 3) == " ".repeat(tabSize) + "jmp")
+            else if (startsWithAnyElement(getLastLineUserWasOn(), exitKeywords))
             {
                 // exited from subroutine - dont type the spaces!
                 console.log("exited subr")
@@ -47,15 +29,53 @@ textarea.addEventListener("keydown", event => {
                 event.preventDefault()
             }
         }
-        else if (event.code == "KeyS" && event.ctrlKey && event.shiftKey)
+        else if (event.code == "Tab")
         {
-            downloadFile()
             event.preventDefault()
-        }
-        else if (event.code == "KeyO" && event.ctrlKey && event.shiftKey)
-        {
-            importFile()
-            event.preventDefault()
+            autoType(" ".repeat(tabSize))
         }
     }
 })
+
+let list = document.getElementById("instructionList")
+
+let currentGroupId = 0
+for (const name of Object.keys(INSTRUCTION_LIST))
+{
+    if (name == INSTRUCTION_GROUPS[currentGroupId].from)
+    {
+        list.appendChild(el`div.spacer`())
+
+        if (currentGroupId != 0)
+        {
+            list.appendChild(el`div.spacer`())
+            list.appendChild(el`div.spacer`())
+        }
+
+
+        list.appendChild(el`div.sectionTitle`(
+            el`h1`(
+                INSTRUCTION_GROUPS[currentGroupId].name
+            ).$({style: "font-weight: bold"})
+        ))
+
+        
+
+        if (currentGroupId + 1 != INSTRUCTION_GROUPS.length) currentGroupId++
+    }
+
+    const instr = INSTRUCTION_LIST[name]
+    console.log(instr)
+
+    const nameel = el`h2`(name + " ")
+    list.appendChild(nameel)
+
+    if (instr.params != "")
+    {
+        nameel.appendChild(el`p.code`(instr.params))
+    }
+
+    list.appendChild(el`p`(instr.desc))
+
+    list.appendChild(el`div.spacer`())
+}
